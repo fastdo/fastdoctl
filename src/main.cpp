6c2f448cb6706d29ccf8754454703980e065d55e
WT <zth555@qq.com>
2023-06-11 18:15:45 +0800

    添加命令行参数解析

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,8 @@
 #include "ctl_api_handlers.hpp"
 #include "ctl_web_handlers.hpp"
 
-int main()
+void StartupServer()
 {
-    // 初始化一些库和设置
-    #if defined(__GNUC__) && !defined(WIN32) // on linux
-    winux::Locale::Set("zh_CN.UTF-8");
-    #else
-
-    #endif
-
-    winux::Locale __loc__;
-    //eiengd::GdInit __gdInit__;
-    eiennet::SocketLib __sockInit__;
-
-    #if defined(OS_WIN)
-    ComInit comInit;
-    #endif
-
     ControlCenterApp app;
     // 设置处理器
     SET_API_HANDLER( app, get_os_info );
@@ -31,7 +16,41 @@ int main()
     SET_API_HANDLER( app, get_fastdo_info );
 
     app.run(nullptr);
-    //*/
+}
 
+int StartupMain( CommandLineVars const & cmdVars )
+{
+    Mixed info;
+    CheckEnvVars(&info);
+    cout << info.myJson(true,"  ","\n");
     return 0;
+}
+
+int main( int argc, char const ** argv, char const ** envp )
+{
+    // 初始化一些库和设置
+    #if defined(__GNUC__) && !defined(WIN32) // on linux
+    winux::Locale::Set("zh_CN.UTF-8");
+    #else
+
+    #endif
+
+    winux::Locale __loc__;
+    eiengd::GdInit __gdInit__;
+    eiennet::SocketLib __sockInit__;
+
+    #if defined(OS_WIN)
+    ComInit comInit;
+    #endif
+
+    CommandLineVars cmdVars( argc, argv, "", "", "server" );
+
+    if ( cmdVars.hasFlag("server") )
+    {
+        // 启动Web服务器
+        StartupServer();
+        return 0;
+    }
+
+    return StartupMain(cmdVars);
 }
